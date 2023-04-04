58e8d73753a3b76a51eb6a03746bf0c37db42da3
wujun9 <wujun9@xiaomi.com>
2023-04-04 14:13:21 +0800

    bug-fix commit3

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     print("hello python v1")
     print("hello python v2")
     print("hello python v3")
-    print("bug-fix modify..")
-    print("bug-fix more..")
+    print("bug-fix modify 我又修改了第6行..")
+    print("bug-fix modify 我又修改了第7行..")
